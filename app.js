454851b1605caa94fb7f1550ab54930268171764
lutfiandri <lutfiandri232@gmail.com>
2020-10-26 19:59:13 +0700

    - tanpa refresh - query : -- where -- order -- limit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // create element and render cafes
 const cafeList = document.querySelector("#cafe-list");
 const renderCafe = (doc) => {
+    // cafeList.innerHTML = "";
     const list = `
         <li data-id="${doc.id}">
             <span>${doc.data().name}</span>
@@ -22,12 +23,24 @@ const renderCafe = (doc) => {
 };
 
 // get data
+// db.collection("cafes")
+//     .get()
+//     .then((snapshot) => {
+//         snapshot.forEach((doc) => {
+//             renderCafe(doc);
+//             // console.log(doc.data(), doc.id);
+//         });
+//     });
+// get data (without refresh)
 db.collection("cafes")
-    .get()
-    .then((snapshot) => {
+    .where("city", "==", "Mars")
+    .orderBy("name", "asc")
+    .limit(10)
+    .onSnapshot((snapshot) => {
+        cafeList.innerHTML = "";
         snapshot.forEach((doc) => {
+            // console.log(doc.data());
             renderCafe(doc);
-            // console.log(doc.data(), doc.id);
         });
     });
 
@@ -37,15 +50,15 @@ form.addEventListener("submit", (e) => {
     e.preventDefault();
     let name = form.name.value;
     let city = form.city.value;
-    console.log(name, city);
+    // console.log(name, city);
     if (name !== "" && city !== "") {
         db.collection("cafes").add({
             name: name,
             city: city,
         });
         // bisa ditambah then catch
-        name = "";
-        city = "";
+        form.name.value = "";
+        form.city.value = "";
     }
     // console.log("add clicked");
 });
